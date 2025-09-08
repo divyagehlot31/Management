0df00ae545e667c5b435ae31cba067759727627b
divyagehlot31 <divyagehlot3112@gmail.com>
2025-09-08 14:14:25 +0530

    change port

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.get('/api/test', (req, res) => {
 
 console.log("Server starting...");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4988;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
