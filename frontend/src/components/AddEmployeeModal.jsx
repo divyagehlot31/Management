7e238d38148e99b85f1f70a0ad75b85017efd4b9
divyagehlot31 <divyagehlot3112@gmail.com>
2025-09-10 12:56:18 +0530

    again add employee in admin

--- frontend/src/components/AddEmployeeModal.jsx
+++ frontend/src/components/AddEmployeeModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import API from "../../utils/api";
+import API from "../utils/api"
 
 const AddEmployeeModal = ({ isOpen, onClose, onEmployeeAdded }) => {
   const [formData, setFormData] = useState({
